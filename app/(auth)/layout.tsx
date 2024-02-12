3b40ce4a363d3280220e896f6b624c715fe4ef7a
ekankam <eb.kankam26@gmail.com>
2024-02-11 18:21:02 -0600

    Create auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="min-h-screen w-full bg-brand-dark-blue flex flex-col items-center max-w-screen-2xl mx-auto text-white">
+    <section className="min-h-screen w-full bg-brand-dark-blue flex flex-col items-center max-w-screen-2xl mx-auto text-white">
       <Image
         src="/assets/logo.svg"
         alt="logo"
@@ -12,7 +12,7 @@ function AuthLayout({ children }: Readonly<{ children: React.ReactNode }>) {
         className="my-14"
       />
       {children}
-    </div>
+    </section>
   );
 }
 
