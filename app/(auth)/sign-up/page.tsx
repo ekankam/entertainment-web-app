dd20abfda3bd7ce30641001bea7e3b2bbd4080b9
ekankam <eb.kankam26@gmail.com>
2024-02-11 18:26:46 -0600

    Create sign up form UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,31 @@
+import FormInput from "@/components/form/form-input";
+import FormWrapper from "@/components/form/form-wrapper";
+import Header from "@/components/form/header";
+import { Button } from "@/components/ui/button";
+import { APP_ROUTES } from "@/types";
+import Link from "next/link";
 import React from "react";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <FormWrapper>
+      <Header header="Sign Up" />
+      <form className="h-full">
+        <FormInput type="email" placeholder="Email address" />
+        <FormInput type="password" placeholder="Password" />
+        <FormInput type="password" placeholder="Repeat password" />
+        <Button className="bg-brand-red text-brand-white hover:bg-brand-white hover:text-brand-semi-dark-blue mt-5 mb-4 w-full h-12">
+          Create an account
+        </Button>
+        <p className="text-center text-sm">
+          Already have an account?{" "}
+          <Link href={APP_ROUTES.LOGIN} className="text-brand-red ml-1">
+            Login
+          </Link>
+        </p>
+      </form>
+    </FormWrapper>
+  );
 }
 
 export default SignUp;
