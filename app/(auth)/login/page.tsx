c3012c0a9d21f82a7876a39c047af12dc19c2f3e
ekankam <eb.kankam26@gmail.com>
2024-02-17 15:50:47 -0600

    Validate form inputs

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,19 +1,57 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import FormInput from "@/components/form/form-input";
 import FormWrapper from "@/components/form/form-wrapper";
 import Header from "@/components/form/header";
 import { Button } from "@/components/ui/button";
 import { APP_ROUTES } from "@/types";
-import Link from "next/link";
-import React from "react";
+import {
+  loginSchema,
+  loginValidationSchema,
+} from "@/utils/form-validation-schema";
 
 function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginValidationSchema>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<loginValidationSchema> = (data) =>
+    console.log(data);
+
   return (
     <FormWrapper>
       <Header header="Login" />
-      <form className="h-full">
-        <FormInput type="email" placeholder="Email address" />
-        <FormInput type="password" placeholder="Password" />
-        <Button className="bg-brand-red text-brand-white hover:bg-brand-white hover:text-brand-semi-dark-blue mt-5 mb-4 w-full h-12">
+      <form className="h-full" onSubmit={handleSubmit(onSubmit)}>
+        <FormInput
+          type="email"
+          placeholder="Email address"
+          {...register("email")}
+          isError={errors.email?.message ? true : false}
+          validationMessage={errors.email?.message}
+        />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+          isError={errors.password?.message ? true : false}
+          validationMessage={errors.password?.message}
+        />
+        <Button
+          className="bg-brand-red text-brand-white hover:bg-brand-white hover:text-brand-semi-dark-blue mt-5 mb-4 w-full h-12"
+          type="submit"
+        >
           Login to your account
         </Button>
         <p className="text-center text-sm">
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,20 +1,65 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import FormInput from "@/components/form/form-input";
 import FormWrapper from "@/components/form/form-wrapper";
 import Header from "@/components/form/header";
 import { Button } from "@/components/ui/button";
 import { APP_ROUTES } from "@/types";
-import Link from "next/link";
-import React from "react";
+import {
+  signUpSchema,
+  signUpValidationSchema,
+} from "@/utils/form-validation-schema";
 
 function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signUpValidationSchema>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      repeatPassword: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<signUpValidationSchema> = (data) =>
+    console.log(data);
+
   return (
     <FormWrapper>
       <Header header="Sign Up" />
-      <form className="h-full">
-        <FormInput type="email" placeholder="Email address" />
-        <FormInput type="password" placeholder="Password" />
-        <FormInput type="password" placeholder="Repeat password" />
-        <Button className="bg-brand-red text-brand-white hover:bg-brand-white hover:text-brand-semi-dark-blue mt-5 mb-4 w-full h-12">
+      <form className="h-full" onSubmit={handleSubmit(onSubmit)}>
+        <FormInput
+          type="email"
+          placeholder="Email address"
+          {...register("email")}
+          isError={errors.email?.message ? true : false}
+          validationMessage={errors.email?.message}
+        />
+        <FormInput
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+          isError={errors.password?.message ? true : false}
+          validationMessage={errors.password?.message}
+        />
+        <FormInput
+          type="password"
+          placeholder="Repeat password"
+          {...register("repeatPassword")}
+          isError={errors.repeatPassword?.message ? true : false}
+          validationMessage={errors.repeatPassword?.message}
+        />
+        <Button
+          className="bg-brand-red text-brand-white hover:bg-brand-white hover:text-brand-semi-dark-blue mt-5 mb-4 w-full h-12"
+          type="submit"
+        >
           Create an account
         </Button>
         <p className="text-center text-sm">
