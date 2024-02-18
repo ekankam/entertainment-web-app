98cb699f8c40efa981d7cec39d12a16052a8cf50
ekankam <eb.kankam26@gmail.com>
2024-02-18 12:52:24 -0600

    Implement Query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "@/lib/query-client-provider";
 
 const outfit = Outfit({
   subsets: ["latin", "latin-ext"],
@@ -27,7 +28,9 @@ export default function RootLayout({
           type="image/x-icon"
         />
       </head>
-      <body className={outfit.className}>{children}</body>
+      <body className={outfit.className}>
+        <QueryProvider>{children}</QueryProvider>
+      </body>
     </html>
   );
 }
