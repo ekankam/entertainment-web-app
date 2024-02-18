920b85289f78dc464ee9b444a6201eb47bf33dba
ekankam <eb.kankam26@gmail.com>
2024-02-17 21:24:14 -0600

    Add User types

--- types/index.ts
+++ types/index.ts
@@ -3,4 +3,12 @@ enum APP_ROUTES {
   SIGN_UP = "/sign-up",
 }
 
+interface User {
+  email: string;
+  password: string;
+  profileColor: string;
+  createdAt: Date;
+}
+
 export { APP_ROUTES };
+export { type User };
